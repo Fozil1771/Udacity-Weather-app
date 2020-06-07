a7d0d08b12177334004265d9074deb30e4d14b80
Fozil1771 <51485736+Fozil1771@users.noreply.github.com>
2020-06-07 08:00:34 +0500

    Update server.js

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -1,5 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 const express = require('express');
@@ -24,7 +24,7 @@ const port = 8000;
 app.listen(port, listening);
 // Setup Server
 function listening(){
-    console.log(`running on localhost: ${port}`);
+    console.log(`Running on localhost: ${port}`);
 }
 
 app.get('/allData', function(req, res){
